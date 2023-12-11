35ce1f3d54930bd188ae30111507557916fe8d87
olddeala <113938189+olddeala89@users.noreply.github.com>
2023-12-11 11:58:44 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -83,7 +83,7 @@ void menu(int& choose) {
                 nowActive--;
             }
             else {
-                nowActive = 5;
+                nowActive = 4;
             }
         }
         else if (pressed == 13) {
@@ -101,35 +101,39 @@ void addTask(std::vector<Task>& vect, int& count);
 //Вывод списка задач
 void listTasks(std::vector<Task>& vect);
 
+//Редактирование задачи
+void editTask(std::vector<Task>& vect);
+
+//Удаление задачи
+void dropTask(std::vector<Task>& vect);
+
 //Точка входа
 int main() {
     int choose = -1, count = 0;
     system("chcp 65001");
     test();
     std::vector<Task> tasks;
-    menu(choose);
-    while (choose != 4) {
-        switch (choose) {
-            case 0:
-                addTask(tasks, count);
-                choose = -1;
-                break;
-            case 1:
-                listTasks(tasks);
-                choose = -1;
-                break;
-            case 2:
-                break;
-            case 3:
-                break;
-            case 4:
-                break;
-            default:
-                std::cout << "Error!" << std::endl;
-                break;
+    while (true) {
+        menu(choose);
+        if (choose == 0) {
+            addTask(tasks, count);
+            choose = -1;
+        }
+        else if (choose == 1) {
+            listTasks(tasks);
+            choose = -1;
+        } else if (choose == 2) {
+            editTask(tasks);
+            choose = -1;
+        } else if (choose == 3) {
+            dropTask(tasks);
+            choose = -1;
+        } else if (choose == 4) {
+            std::cout << "Программа завершила работу.";
+            system("pause");
+            break;
         }
     }
-
 
     return 0;
     /*int ch = _getch();
@@ -151,15 +155,65 @@ void addTask(std::vector<Task>& vect, int& count) {
     std::cin.ignore(1, '\n');
     vect.push_back(Task(count, task, desc, prio));
     count++;
+    std::cout << "Задача успешно добавленна!" << std::endl;
+    system("pause");
 }
 
 //Вывод списка задач
 void listTasks(std::vector<Task>& vect) {
+    system("cls");
     std::cout << "Ваши задачи: " << std::endl;
     for (int i = 0; i < vect.size(); i++) {
-        std::cout << vect[i].getId() << "." << vect[i].getName() << std::endl << vect[i].getDescription()
-        << std::endl << "Приоретет: " << vect[i].getPriority() << std::endl << std::endl;
+        std::cout << i+1 << "." << vect[i].getName() << std::endl << vect[i].getDescription()
+        << std::endl << "Приоретет: " << vect[i].getPriority() << " ID: " <<vect[i].getId() << std::endl << std::endl;
     }
+    system("pause");
+}
+
+//Редактирование задачи
+void editTask(std::vector<Task>& vect) {
+    system("cls");
+    listTasks(vect);
+    int point;
+    std::cout << "Какую задачу хотите отрадактировать (ID): ";
+    std::cin >> point;
+    std::cin.ignore(1, '\n');
+    std::cout << "\nВведите новую задачу:\n";
+    std::string task;
+    std::getline(std::cin, task);
+    std::cout << "Введите новое описание: \n";
+    std::string desc;
+    std::getline(std::cin, desc);
+    std::cout << "Введите новый приоритет задачи (0 - малый, 1 - средний, 2 - высокий): ";
+    int prio;
+    std::cin >> prio;
+    std::cin.ignore(1, '\n');
+    vect.at(point).setName(task);
+    vect.at(point).setDescription(desc);
+    vect.at(point).setPriority(prio);
+    std::cout << "\nЗадача изменена!\n";
+    system("pause");
+}
+
+//Удаление задачи
+void dropTask(std::vector<Task>& vect) {
+    system("cls");
+    listTasks(vect);
+    int point;
+    size_t indexToRemove = 0;
+    std::cout << "Какую задачу хотите удалить (ID): ";
+    std::cin >> point;
+    std::cin.ignore(1, '\n');
+    for (int i = 0; i < vect.size(); i++) {
+        if (vect[i].getId() == point) {
+            indexToRemove = i;
+        }
+    }
+    if (indexToRemove < vect.size()) {
+        vect.erase(vect.begin() + indexToRemove);
+    }
+    std::cout << "Задача удалена!\n";
+    system("pause");
 }
 
 //Вниз 115
